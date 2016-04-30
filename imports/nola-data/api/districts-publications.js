3c1c5e9d3609dbe071f29416023f4b9952a66d6c
Marvin Arnold <marvinmarnold@gmail.com>
2016-04-30 14:50:15 -0500

    Toggle district layer

--- imports/nola-data/api/districts-publications.js
+++ imports/nola-data/api/districts-publications.js
@@ -2,6 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import { Districts } from '../collections/districts.js';
 
 Meteor.publish('districts', () => {
-  console.log('publish districts');
   return Districts.find();
 });
--- imports/nola-data/client/ui/pages/NOPDResponseTime.jsx
+++ imports/nola-data/client/ui/pages/NOPDResponseTime.jsx
@@ -13,16 +13,44 @@ class NOPDResponseTime extends Component {
     };
   }
 
+  viewPoliceDistricts() {
+    this.setState({
+      view: STATES.VIEW.POLICE_DISTRICTS
+    });
+  }
+
+  viewCensusTracts() {
+    this.setState({
+      view: STATES.VIEW.CENSUS_TRACTS
+    });
+  }
+
+  renderDistrictLayer() {
+    _.each(this.props.districts, district => {
+      const districtLatLngs = _.map(district.boundaries, boundary => {
+        return [boundary.latitude, boundary.longitude]
+      });
+
+      L.polygon(districtLatLngs, {color: "#000000"})
+        .bindLabel(district.name)
+        .addTo(districtLayer);
+    });
+  }
+
+  renderTractLayer() {
+    console.log('renderTract');
+  }
+
   renderBoundaryType() {
     return (
       <div>
         <h3>Select View</h3>
         <div className="btn-group" data-toggle="buttons">
-          <label className="btn btn-primary active">
-            <input type="radio" name="options" id="option1" autocomplete="off" defaultChecked /> Police Districts
+          <label className="btn btn-primary active" onClick={this.viewPoliceDistricts.bind(this)} >
+            <input type="radio" name="options" id="view-police-districts-radio" defaultChecked /> Police Districts
           </label>
-          <label className="btn btn-primary">
-            <input type="radio" name="options" id="option2" autocomplete="off" /> Census Tracts
+          <label className="btn btn-primary" onClick={this.viewCensusTracts.bind(this)} >
+            <input type="radio" name="options" id="view-census-tracts-radio" /> Census Tracts
           </label>
         </div>
       </div>
@@ -30,6 +58,16 @@ class NOPDResponseTime extends Component {
   }
 
   render() {
+    if(!this.props.loading) {
+      this.props.districtLayer.setGeoJSON([]);
+
+      if(this.state.view === STATES.VIEW.POLICE_DISTRICTS) {
+        this.renderDistrictLayer();
+      } else if(this.state.view === STATES.VIEW.CENSUS_TRACTS) {
+        this.renderTractLayer();
+      }
+    }
+
     return (
       <div className="container-fluid">
         <div className="row">
@@ -50,46 +88,41 @@ class NOPDResponseTime extends Component {
 
 NOPDResponseTime.propTypes = {
   districts: React.PropTypes.array,
-  loading: React.PropTypes.bool
+  loading: React.PropTypes.bool,
+  map: React.PropTypes.object,
+  districtLayer: React.PropTypes.object
 };
 
 let map, districtLayer;
 export default createContainer(() => {
-  
+
   const districtsHandle = Meteor.subscribe('districts');
   const loading = !districtsHandle.ready();
   const districts = Districts.find().fetch();
 
+  const initMap = () => {
+    L.mapbox.accessToken = Meteor.settings.public.MAPBOX_TOKEN;
+
+    map = L.mapbox.map('map', 'mapbox.streets').setView([
+      29.942355,
+      -90.078635
+    ], 12);
+
+    districtLayer = L.mapbox.featureLayer().addTo(map);
+  };
+
   Tracker.autorun(function () {
     if (Mapbox.loaded()) {
       if(!map) {
-        L.mapbox.accessToken = Meteor.settings.public.MAPBOX_TOKEN;
-
-        map = L.mapbox.map('map', 'mapbox.streets').setView([
-          29.942355,
-          -90.078635
-        ], 12);
-
-        districtLayer = L.mapbox.featureLayer().addTo(map);
-      }
-
-      if(map) {
-        districtLayer.setGeoJSON([])
-        _.each(districts, district => {
-          const districtLatLngs = _.map(district.boundaries, boundary => {
-            return [boundary.latitude, boundary.longitude]
-          });
-
-          L.polygon(districtLatLngs, {color: "#000000"})
-            .bindLabel(district.name)
-            .addTo(districtLayer);
-        });
+        initMap();
       }
     }
   });
 
   return {
     districts: districts,
-    loading: loading
+    loading: loading || !map,
+    map: map,
+    districtLayer: districtLayer
   };
 }, NOPDResponseTime);
