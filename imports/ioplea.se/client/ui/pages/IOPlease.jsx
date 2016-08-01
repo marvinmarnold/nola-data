b8155775aa66f6b55a4c336eb8497aaf7765eafa
Marvin Arnold <marvinmarnold@gmail.com>
2016-08-01 13:39:46 +0300

    Bump Meteor version. Add initial styles for ioPleas.se

--- imports/ioplea.se/client/ui/pages/IOPlease.jsx
+++ imports/ioplea.se/client/ui/pages/IOPlease.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Footer from '../components/Footer.jsx';
 import Results from '../components/Results.jsx';
 import Search from '../components/Search.jsx';
 
@@ -18,15 +19,28 @@ export default class Index extends Component {
 
   render() {
     return (
-      <div className="container-fluid text-xs-center">
-        <div className="row">
-          <div className="col-xs-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2">
-            <h1 className='m-t-3'><stong>Do stuff</stong> with your IoT</h1>
+      <div>
+        <div className="ioplease-banner">
+          <div className="container-fluid text-xs-center">
+            <div className="row">
+              <div className="col-xs-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2">
+                <h1 className='m-t-3'><stong>Do stuff</stong> with your IoT</h1>
 
-            <Search setQuery={this.setQuery.bind(this)}/>
-            {this.state.query == "" ? null : <Results query={this.state.query} thingId={this.state.thingId} />}
+                <Search setQuery={this.setQuery.bind(this)}/>
+              </div>
+            </div>
           </div>
         </div>
+
+        <div className="container-fluid text-xs-center">
+          <div className="row">
+            <div className="col-xs-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2">
+              {this.state.query == "" ? null : <Results query={this.state.query} thingId={this.state.thingId} />}
+            </div>
+          </div>
+        </div>
+
+        <Footer />
       </div>
     );
   }
